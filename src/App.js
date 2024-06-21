459cb6c9af0d68539033609cffbbb6efff0ce3fe
tapsTapayan <tnashuriel@gmail.com>
2024-06-21 19:05:10 +0800

    [Adjustment][Taps] Wala lang

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           Learn daw ug React
         </a>
