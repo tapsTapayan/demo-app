8d57cd31b32fa7a3c263a5738caa5d7f75ed1d76
Jay <thisiszammy@gmail.com>
2024-06-21 19:03:53 +0800

    [UI Change][Zammy] Changed App.js UI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Changed App.js Paragraph (Zammy was here)
         </p>
         <a
           className="App-link"
